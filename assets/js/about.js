0d3436ab2bf1f36b1de01e259f14ee1fa7366013
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-02-21 23:39:06 +0100

    Added github activity. Github calendar.

---
+++ assets/js/about.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+   /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    new GitHubCalendar("#github-graph", "JaviBodas");
+    
+    
+    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+    //GitHubActivity.feed({ username: "javibodas", selector: "#ghfeed" });
+});
