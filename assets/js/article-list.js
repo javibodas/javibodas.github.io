b4a6515bf57cd87f1fd65c9a22be1fd87bcb02c3
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-03-07 14:57:34 +0100

    Change origin data articles from static lists to json string simulating api data.

--- assets/js/article-list.js
+++ assets/js/article-list.js
@@ -7,7 +7,7 @@ class ArticlesList extends React.Component {
     super(props);
     // This info should be obtained by api 
     this.months = Array('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
-    this.data = JSON.parse('[{"year":"2020", "months":[{"month": "Febrero","articles" : ["Aplicaciones Descentralizadas. Instalación y configuración"]},{"month": "Marzo", "articles" : ["Aplicación Spring Boot en Heroku. Instalación y Configuración"]}]}]');
+    this.data = JSON.parse('[{"year":"2020", "months":[{"month": "Febrero","articles":[{"title":"Aplicaciones Descentralizadas. Instalación y configuración", "link":"public/posts/configuracion-dapp.html"}]},{"month": "Marzo", "articles" : [{"title":"Aplicación Spring Boot en Heroku. Instalación y Configuración","link":"public/posts/iniciar-proyecto-spring-y-heroku.html"}]}]}]');
 
     this.current_month = months[(new Date()).getMonth()];
     this.current_year = (new Date()).getFullYear();
@@ -21,24 +21,24 @@ class ArticlesList extends React.Component {
     let list = [];
     let years_articles = this.data;
 
-    for(var i = 0; i < this.years_articles.length; i++){
+    for(var i = 0; i < years_articles.length; i++){
         let months_html = [];
         let months_articles = years_articles[i].months;        
 
-        for(var j = 0; j < this.months_articles.length; j++){
+        for(var j = 0; j < months_articles.length; j++){
             let articles_html = [];
             let articles_title = months_articles[0].articles;
             
-            for(var k = 0; k < this.articles_title.length; k++){
-              articles_html.push(<li><a>{this.articles_title[k]}</a></li>);
+            for(var k = 0; k < articles_title.length; k++){
+              articles_html.push(<li><a href={articles_title[k].link}>{articles_title[k].title}</a></li>);
             }
 
-          if(this.months_articles[j].month == this.current_month) months_html.push(<li><span className='caret month-articles caret-down'>{this.months_articles[j].month}</span><ul className='nested active'>{articles_html}</ul></li>);
-          else months_html.push(<li><span className='caret month-articles' onClick={() => this.click_caret()}>{this.months_articles[j].month}</span><ul className='nested'>{articles_html}</ul></li>);
+          if(months_articles[j].month == this.current_month) months_html.push(<li><span className='caret month-articles caret-down'>{months_articles[j].month}</span><ul className='nested active'>{articles_html}</ul></li>);
+          else months_html.push(<li><span className='caret month-articles'>{months_articles[j].month}</span><ul className='nested'>{articles_html}</ul></li>);
         }
 
-      if(this.years_articles[i].year == this.current_year) list.push(<li><span className='caret year-articles caret-down'>{this.years_articles[i].year}</span><ul className='nested active'>{months_html}</ul></li>);
-      else list.push(<li><span className='caret year-articles' onClick={() => this.click_caret()}>{this.years_articles[i].year}</span><ul className='nested'>{months_html}</ul></li>);
+      if(years_articles[i].year == this.current_year) list.push(<li><span className='caret year-articles caret-down'>{years_articles[i].year}</span><ul className='nested active'>{months_html}</ul></li>);
+      else list.push(<li><span className='caret year-articles'>{years_articles[i].year}</span><ul className='nested'>{months_html}</ul></li>);
       
     }
     return list;
@@ -59,4 +59,5 @@ for (var i = 0; i < toggler.length; i++) {
     toggler[i].addEventListener("click", function() {
       this.parentElement.querySelector(".nested").classList.toggle("active");
       this.classList.toggle("caret-down");
-});
+  });
+}
