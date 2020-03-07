c88ce94ef319f11b043cb764769385612d500a72
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-03-07 20:55:29 +0100

    Hide post iniciar-proyecto-spring-heroku

--- assets/js/article-list.js
+++ assets/js/article-list.js
@@ -7,7 +7,9 @@ class ArticlesList extends React.Component {
     super(props);
     // This info should be obtained by api 
     this.months = Array('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
-    this.data = JSON.parse('[{"year":"2020", "months":[{"month": "Febrero","articles":[{"title":"Aplicaciones Descentralizadas. Instalación y configuración", "link":"/public/posts/configuracion-dapp.html"}]},{"month": "Marzo", "articles":[{"title":"Aplicación Spring Boot en Heroku. Instalación y Configuración","link":"/public/posts/iniciar-proyecto-spring-y-heroku.html"}]}]}]');
+    this.data = JSON.parse('[{"year":"2020", "months":[{"month": "Febrero","articles":[{"title":"Aplicaciones Descentralizadas. Instalación y configuración", "link":"/public/posts/configuracion-dapp.html"}]}]}]');
+    //this.data = JSON.parse('[{"year":"2020", "months":[{"month": "Febrero","articles":[{"title":"Aplicaciones Descentralizadas. Instalación y configuración", "link":"/public/posts/configuracion-dapp.html"}]},{"month": "Marzo", "articles":[{"title":"Aplicación Spring Boot en Heroku. Instalación y Configuración","link":"/public/posts/iniciar-proyecto-spring-y-heroku.html"}]}]}]');
+
 
     this.current_month = this.months[(new Date()).getMonth()];
     this.current_year = (new Date()).getFullYear();
