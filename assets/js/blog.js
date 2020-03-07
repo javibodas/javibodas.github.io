da9377beb5f6e1542a86f9ac85405f6f9c24851d
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-03-07 15:38:44 +0100

    Change origin data articles from static lists to json string simulating api data. Testing in production.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,24 +1,6 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('pre code').forEach((block) => {
-      hljs.highlightBlock(block);
-    });
-
-    var toggler = document.getElementsByClassName("caret");
-    var i;
-
-for (i = 0; i < toggler.length; i++) {
-  toggler[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".nested").classList.toggle("active");
-    this.classList.toggle("caret-down");
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block);
   });
-
-  if(toggler[i].textContent == '2020'){
-    toggler[i].parentElement.querySelector(".nested").classList.toggle("active");
-    toggler[i].classList.toggle("caret-down");
-  }else if(toggler[i].textContent == 'Febrero'){
-    toggler[i].parentElement.querySelector(".nested").classList.toggle("active");
-    toggler[i].classList.toggle("caret-down");
-  }
-}
 });
 
