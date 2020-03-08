56e846d7633fce33efcc23e1d04fbad2aa702a76
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-03-08 18:32:33 +0100

    Testing posts list in mobile version with react.

---
+++ assets/js/article-mobile-list.js
@@ -0,0 +1,60 @@
+'use strict';
+
+class ArticleMobileList extends React.Component{
+	constructo(super){
+		this.data = JSON.parse('[{"title":"Aplicaciones Descentralizadas. Instalación y configuración", "link":"/public/posts/configuracion-dapp.html","publication_date":"08/03/2020","active" : 0,"description":"En esta guía se mostrarán los pasos a seguir para crear una DAPP (Aplicación Descentralizada) basada en contratos inteligentes con la herramienta Truffle y blockchain con Ethereum. En esta primera parte se llevará a cabo la configuración del entorno de desarrollo."},{"title":"Aplicación Spring Boot en Heroku. Instalación y Configuración","publication_date":"08/03/2020","link":"/public/posts/iniciar-proyecto-spring-y-heroku.html", "active" : 1,"description":""}]');
+	}
+
+	 create_list() {
+	    let list = [];
+	    let articles = this.data;
+
+	    for(var i = 0; i < articles.length; i++){
+	      list.push(<Post title={articles[i].title} publication_date={articles[i].publication_date} description={articles[i].description} link={articles[i].link}/>);
+	    }
+	    return list;
+	}
+
+	render() {
+		return(<>{create_list()}</>);
+	}
+}
+
+class NextPrevNav extends React.Component {
+  render() {
+    return (<nav className='blog-nav nav nav-justified my-5'>
+									  <a className='nav-link-prev nav-item nav-link d-none rounded-left' href='#'>Previous<i className='arrow-prev fas fa-long-arrow-alt-left'></i></a>
+									  <a className='nav-link-next nav-item nav-link rounded' href='blog-list.html'>Next<i className='arrow-next fas fa-long-arrow-alt-right'></i></a>
+									</nav>);
+  }
+}
+
+//<div class="item mb-5">
+//<div class="media">
+//	<!--<img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="/assets/images/blog/blog-post-thumb-1.jpg" alt="image">-->
+//	<div class="media-body">
+//		<h3 class="title mb-1"><a href="/public/posts/configuracion-dapp.html">Desarrollo de Aplicaciones Descentralizadas (DAPP). Parte I - Instalación y Configuración</a></h3>
+//		<div class="meta mb-1"><span class="date">Publicado 02/02/2020</span><!--<span class="time">5 min read</span><span class="comment"><a href="#">8 comments</a></span>--></div>
+//		<div class="intro">En esta guía se mostrarán los pasos a seguir para crear una DAPP (Aplicación Descentralizada) basada en contratos inteligentes con la herramienta Truffle y blockchain con Ethereum. En esta primera parte se llevará a cabo la configuración del entorno de desarrollo.</div>
+//		<a class="more-link" href="/public/posts/configuracion-dapp.html"> Leer más &rarr;</a>
+//	</div>
+//</div>
+//</div>
+
+class Post extends React.Component {
+	render(){
+		return(<div class="item mb-5"><div class="media"><div class="media-body">
+					<h3 class="title mb-1"><a href="/public/posts/configuracion-dapp.html">{this.props.title}</a></h3>
+					<div class="meta mb-1"><span class="date">Publicado {this.props.publication_date}</span><!--<span class="time">5 min read</span><span class="comment"><a href="#">8 comments</a></span>--></div>
+					<div class="intro">{this.props.description}</div>
+					<a class="more-link" href={this.props.link}> Leer más &rarr;</a>
+				</div>
+				</div>
+				</div>);
+	}
+}
+
+const e = React.createElement;
+const domContainer = document.querySelector('.article-mobile-list');
+
+ReactDOM.render(e(ArticlesTreeList), domContainer);
---
+++ assets/js/article-tree-list.js
@@ -0,0 +1,83 @@
+'use strict';
+
+class ArticlesTreeList extends React.Component {
+  constructor(props) {
+    super(props);
+    // This info should be obtained by api 
+    this.data = JSON.parse('[{"year":"2020", "months":[{"month": "Febrero","articles":[{"title":"Aplicaciones Descentralizadas. Instalación y configuración", "link":"/public/posts/configuracion-dapp.html", "active" : 0}]},{"month": "Marzo", "articles":[{"title":"Aplicación Spring Boot en Heroku. Instalación y Configuración","link":"/public/posts/iniciar-proyecto-spring-y-heroku.html", "active" : 1}]}]}]');
+  }
+
+  get_articles(){
+    
+  }
+
+  create_list() {
+    let list = [];
+    let active = 0; // This variable is used to expand the tree in month and year of article with active=1 
+    let years_articles = this.data;
+
+    for(var i = 0; i < years_articles.length; i++){
+        let months_html = [];
+        let months_articles = years_articles[i].months;        
+
+        for(var j = 0; j < months_articles.length; j++){
+            let articles_html = [];
+            let articles_title = months_articles[j].articles;
+            
+            for(var k = 0; k < articles_title.length; k++){
+              active = articles_title[k].active;
+              articles_html.push(<Article link={articles_title[k].link} title={articles_title[k].title}/>);
+            }
+
+          months_html.push(<Month month={months_articles[j].month} artics={articles_html} active={active}/>);
+        }
+
+      list.push(<Year year={years_articles[i].year} months={months_html} active={active}/>);
+    }
+    return list;
+  }
+
+  render() {
+    return (this.create_list());
+  }
+}
+
+class Article extends React.Component{
+
+  render(){
+    return (<li><a href={this.props.link}>{this.props.title}</a></li>);
+  }
+
+}
+
+class Month extends React.Component{
+
+  render(){
+    if(this.props.active == 1) return(<li><span className='caret month-articles caret-down'>{this.props.month}</span><ul className='nested active'>{this.props.artics}</ul></li>);
+    else return(<li><span className='caret month-articles'>{this.props.month}</span><ul className='nested'>{this.props.artics}</ul></li>);   
+  }
+
+}
+
+class Year extends React.Component{
+
+  render(){
+    if(this.props.active == 1) return(<li><span className='caret year-articles caret-down'>{this.props.year}</span><ul className='nested active'>{this.props.months}</ul></li>);
+    else return(<li><span className='caret year-articles'>{this.props.year}</span><ul className='nested'>{this.props.months}</ul></li>);
+  }
+
+}
+
+const e = React.createElement;
+const domContainer = document.querySelector('.article-list');
+
+ReactDOM.render(e(ArticlesTreeList), domContainer);
+
+// This sould be replaced by code in react using react elements instead html elements.
+var toggler = document.getElementsByClassName("caret");
+for (var i = 0; i < toggler.length; i++) {
+    toggler[i].addEventListener("click", function() {
+      this.parentElement.querySelector(".nested").classList.toggle("active");
+      this.classList.toggle("caret-down");
+  });
+}
