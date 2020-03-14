d32281abc3b855fe0d61d9b429ac6043f207b55e
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-03-14 13:06:54 +0100

    Modify state tree nodes with react onclick. Testing...

--- assets/js/article-tree-list.js
+++ assets/js/article-tree-list.js
@@ -63,9 +63,8 @@ class SpanClickable extends React.Component{
   }
 
   componentDidMount() {
-    console.log('Mount with' + this.props.open + '-' + this.props.text);
-    this.props.open ? this.setState({open: 0, spanClass: 'caret year-articles', ulClass: 'nested'})
-                  : this.setState({open: 1, spanClass: 'caret year-articles caret-down', ulClass: 'nested active'})
+    this.props.open ? this.setState({open: 1, spanClass: 'caret year-articles caret-down', ulClass: 'nested active'})
+                  : this.setState({open: 0, spanClass: 'caret year-articles', ulClass: 'nested'})
   }
 
   handleClick(){
