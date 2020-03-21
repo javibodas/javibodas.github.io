67d4430b41952cee7c22eb2e1e88c652036c8e35
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-03-21 13:03:41 +0100

    Reoganize react components

--- assets/js/components/desktop.js
+++ assets/js/components/desktop.js
@@ -42,8 +42,8 @@ class Post extends React.Component{
     	} else if (!isLoaded) {
       		return <Loading />;
     	} else {
-    		let post = article.content.replace('&lt;','<').replace('&gt;','>');
-			return(<div dangerouslySetInnerHTML={{__html: marked(article.content, {sanitize: true})}}></div>);
+    		let post = '<p>El objetivo de este post es mostrar los <label class=\"step\">pasos</label> a seguir para crear una <b><a href=\"https://www.miethereum.com/smart-contracts/dapps/\">DAPP (Aplicación Descentralizada)</a></b>.La aplicación de ejemplo que se va a mostrar permitirá generar y gestionar contratos de servicios móviles desde el navegador. Para ello utilizaremos las herramientas: <b><a href=\"https://www.trufflesuite.com/\">Truffle Suite</a></b> con la que podremos crear y hacer uso de <a href=\"https://www.miethereum.com/smart-contracts/\">contratos inteligentes</a> a través de <b><a href=\"https://ethereum.org/\">Ethereum</a></b>; y <b><a href=\"https://metamask.io/\">MetaMask</a></b> para realizarlo desde el navegador. El sistema operativo que se va a utilizar en este ejemplo es <b><a href=\"https://ubuntu.com/download/desktop\">Ubuntu 18.04</a></b>. </p>';
+			return(<div dangerouslySetInnerHTML={{__html: post}}></div>);
 		}
 	}		
 
