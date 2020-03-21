5f33c82873cf4f25848e260d777dde908596903a
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2020-03-21 16:33:03 +0100

    Change almost all html elements in index.html to react components. Testing

--- assets/js/components/desktop.js
+++ assets/js/components/desktop.js
@@ -2,7 +2,7 @@
 
 class DesktopVersion extends React.Component{
 	render() {
-		return(<div class="row desktop">
+		return(<React.Fragment>
 						<div class="col-lg-9 col-12"><Post /></div>
 						<div class="col-lg-3 col-12 right-bar">
 							<div class="px-3 p-md-5">
@@ -12,7 +12,7 @@ class DesktopVersion extends React.Component{
 								</div>
 							</div>
 						</div>
-				</div>);
+				<React.Fragment>);
 	}
 }
 
